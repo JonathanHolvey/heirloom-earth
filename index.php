0fc41956719558830dfdf30b0cfc2dfc71084b6d
Jonathan Holvey <jonathan.holvey@outlook.com>
2015-07-05 10:07:39 +1000

    renamed favicon, created placeholder landing page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Heirloom Earth</title>
+	<meta charset="UTF-8"/>
+	<meta name="description" content=""/>
+	<meta name="keywords" content=""/>
+	<meta name="author" content="Jonathan Holvey"/>
+	<link rel="shortcut icon" href="images/icon.ico"/>
+	<style type="text/css">
+		html, body {
+			height: 100%;
+			margin: 0;
+		}
+		body {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+		}
+		img {
+			display: block;
+			height: 350px;
+			margin-top: -20px;
+		}
+	</style>
+</head>
+<body>
+		<img src="images/logo.svg" alt="Heirloom Earth"/>
+</body>
+</html>
