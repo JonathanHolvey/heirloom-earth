54c9d8f5a8d47e05851d65b048d0d88c529a1577
Jonathan Holvey <jonathan.holvey@outlook.com>
2015-07-05 22:52:35 +1000

    added web fonts and created dummy page header

--- index.php
+++ index.php
@@ -6,26 +6,21 @@
 	<meta name="description" content=""/>
 	<meta name="keywords" content=""/>
 	<meta name="author" content="Jonathan Holvey"/>
-	<meta name="google-site-verification" content="qg5eV80UdswQZ2s2lbXuPPFI0W70Z3PdSwPjz__VRn4" />
-	<link rel="shortcut icon" href="images/icon.ico"/>
-	<style type="text/css">
-		html, body {
-			height: 100%;
-			margin: 0;
-		}
-		body {
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-		}
-		img {
-			display: block;
-			height: 350px;
-			margin-top: -20px;
-		}
-	</style>
+	<meta name="google-site-verification" content="qg5eV80UdswQZ2s2lbXuPPFI0W70Z3PdSwPjz__VRn4"/>
+	<?php include("resources.php") ?>
 </head>
 <body>
-	<img src="images/logo.svg" alt="Heirloom Earth"/>
+	<header class="page-header">
+		<img class="header-logo" src="images/logo.svg" alt="Heirloom Earth"/>
+		<ul class="header-nav">
+			<li>Home</li>
+			<li class="active">Gardening</li>
+			<li>About</li>
+			<li>Contact</li>
+		</ul>
+	</header>
+	<div class="content">
+
+	</div>
 </body>
 </html>
---
+++ resources.php
@@ -0,0 +1,4 @@
+<link rel="shortcut icon" href="images/icon.ico"/>
+<link rel="stylesheet" type="text/css" href="styles/normalize.css"/>
+<link rel="stylesheet" type="text/css" href="styles/fonts.css"/>
+<link rel="stylesheet" type="text/css" href="styles/common.css"/>
