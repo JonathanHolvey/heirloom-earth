f7bfb060aec898c65dac6d3dc37b88bcea326fa4
Jonathan Holvey <jonathan.holvey@outlook.com>
2015-07-11 20:28:18 +1000

    created styles for dummy blog post

--- index.php
+++ index.php
@@ -12,15 +12,30 @@
 <body>
 	<header class="page-header">
 		<img class="header-logo" src="images/logo.svg" alt="Heirloom Earth"/>
-		<ul class="header-nav">
-			<li>Home</li>
-			<li class="active">Gardening</li>
-			<li>About</li>
-			<li>Contact</li>
-		</ul>
+		<nav class="header-nav">
+			<a>Home</a>
+			<a class="active">Gardening</a>
+			<a>About</a>
+			<a>Contact</a>
+		</nav>
 	</header>
-	<div class="content">
-
-	</div>
+	<main class="post">
+		<header>
+			<h1>The herbs in my garden are delicious</h1>
+			<time>5 July 2015</time>
+		</header>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id ornare erat, et sollicitudin velit. Aliquam erat volutpat.</p>
+		<p>
+			<img src="https://scontent-lax1-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/11018919_10152972431403597_1852821027081922794_n.jpg?oh=cf07161faadc63ba4fa6202b0b85f1b4&amp;oe=5628A0B7" alt="Parsley photo"/>
+			<em>I took this arty photo of my parsley</em>
+		</p>
+		<p>Proin accumsan orci in purus commodo interdum. Sed elementum congue libero et venenatis. Quisque ac nibh vel ipsum cursus ornare ac id tellus. Proin nec ante at est varius elementum. Suspendisse pulvinar #vestibulum porttitor. Sed varius purus ex, sed tempus felis placerat nec. Duis in efficitur metus.</p>
+		<p>uisque a finibus elit, id porta lorem.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam bibendum eros ut aliquet gravida. Proin lectus lectus, hendrerit ac sapien non, consectetur vestibulum lacus. Vivamus vel #scelerisque massa. Duis molestie lacinia leo eget interdum. Sed porttitor lacus sit amet tincidunt tempus.</p>
+		<p>Maecenas sed nunc facilisis, ullamcorper tortor sed, porta mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc dapibus elit vel imperdiet ultricies.</p>
+		<p>Vestibulum hendrerit purus urna, nec mollis arcu euismod id. Nunc id interdum diam. Suspendisse dolor nunc, placerat ac tempor a, condimentum eu nisi. Ut pretium at quam id vehicula.</p>
+		<hr/>
+		<footer class="category">From category: <a class="property">gardening</a></footer>
+		<footer class="tags">Tagged as: <a class="property">herbs</a> <a class="property">gardeningtips</a> <a class="property">goodfood</a></footer>
+	</main>
 </body>
 </html>
