5d7b9b08c917067502d867bf438dc6c80ea52477
Jonathan Holvey <jonathan.holvey@outlook.com>
2015-07-15 21:12:02 +1000

    added styles for h2-h6, ul, ol, blockquote

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,3 @@
 <footer class="page-footer">
-	&copy; Heirloom Earth <?= date("Y") ?> - all rights reserved &emsp;&emsp;Website by <a href="http://rocketchilli.com">Rocket Chilli</a>
+	&copy; Heirloom Earth <?= date("Y") ?> - all rights reserved&emsp;&emsp;Website by <a href="http://rocketchilli.com">Rocket Chilli</a>
 </footer>
