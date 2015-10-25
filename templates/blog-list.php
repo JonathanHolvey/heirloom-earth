dfa23885178920e26a6bf53f8cdd455610cf6766
Jonathan Holvey <jonathan.holvey@outlook.com>
2015-10-25 16:50:49 +1000

    Reformat post list with block hyperlink and added metadata

--- templates/blog-list.php
+++ templates/blog-list.php
@@ -2,9 +2,18 @@
 	<h1><?= isset($_GET["f"]) ? preg_replace("/s$/", "", $_GET["f"]) . ": " . $_GET["m"] : "All posts" ?></h1>
 	<?php foreach ($blogList->list as $post): ?>
 		<article>
-			<div class="image"><a href="posts/<?= $post->permalink ?>"><img alt src="<?= $post->image == "" ? "images/default-thumb.svg" : $post->image ?>"/></a></div>
-			<div class="info"><h2><a href="posts/<?= $post->permalink ?>"><?= $post->title ?></a></h2>
-			<?= $post->intro ?></div>
+			<a href="posts/<?= $post->permalink ?>">
+				<div class="image"><img alt src="<?= $post->image == "" ? "images/default-thumb.svg" : $post->image ?>"/></div>
+				<div class="info">
+					<h2><?= $post->title ?></h2>
+					<div class="meta">
+						<div class="property">Published: <span class="value"><?= date("j M Y", strtotime($post->created)) ?></span></div>
+						<div class="property">Category: <span class="value"><?= $post->category ?></span></div>
+						<div class="property">Tags: <span class="value">#<?= implode(" #", $post->tags) ?></span></div>
+					</div>
+					<?= $post->intro ?>
+				</div>
+			</a>
 		</article>
 	<?php endforeach ?>
 </main>
