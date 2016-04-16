2ff5e71d1f6f4f383e5dc84117d07ba38ab9299a
Jonathan Holvey <jonathan.holvey@outlook.com>
2016-04-16 16:29:22 +1000

    Add work in progress notice to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,5 @@
 <footer class="page-footer">
+	<span>This site is a work in progress. Please contact me if you find something that doesn't work</span>
 	<span>&copy; Heirloom Earth <?= date("Y") ?> - all rights reserved</span>
 	<span>Website by <a href="http://rocketchilli.com">Rocket Chilli</a></span>
 </footer>
