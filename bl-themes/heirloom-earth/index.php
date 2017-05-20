6053cf64c11355766e567da10f0166d230a438cb
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-05-20 16:30:09 +1000

    Create structure for Bludit theme

---
+++ bl-themes/heirloom-earth/index.php
@@ -0,0 +1,4 @@
+<?php
+	if ($Url->whereAmI() == "home")
+		include_once(PATH_THEME_PHP . "home.php");
+?>
---
+++ bl-themes/heirloom-earth/php/home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?= $Site->title() ?></title>
+	<?php include(PATH_THEME_PHP . "_head.php") ?>
+</head>
+<body>
+	<div class="container">
+		<header>
+			<img class="logo" src="<?= HTML_PATH_THEME_IMG . "header-logo.svg" ?>" alt="Heirloom Earth"/>
+			<nav>
+				<a class="active" href="">Blog</a>
+				<a href="">Categories</a>
+				<a href="">About</a>
+				<a href="">Contact</a>
+			</nav>
+		</header>
+		<section class="about">
+			<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</h1>
+			<img class="avatar" src="" alt=""/>
+			<a class="button light" href="">Find me on Facebook</a>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+		</section>
+		<section class="blog-preview">
+			<article>
+				<img src="" alt=""/>
+				<h1>The hierarchy of consumption</h1>
+				<div class="post-info">
+				<p>Since starting my sustainability journey, the biggest challenge for me has been pairing my love for the planet with my love for beautiful interior decorating. I've had this passion since a very young age; as a child I would spend weekends dragging my furniture around my bedroom.</p>
+				<a class="button dark" href="">Read more</a>
+			</article>
+			<article>
+				<img src="" alt=""/>
+				<h1>The hierarchy of consumption</h1>
+				<div class="post-info">
+				<p>Since starting my sustainability journey, the biggest challenge for me has been pairing my love for the planet with my love for beautiful interior decorating. I've had this passion since a very young age; as a child I would spend weekends dragging my furniture around my bedroom.</p>
+				<a class="button dark" href="">Read more</a>
+			</article>
+		</section>
+		<section class="instagram block-grid">
+			<div class="block">
+			<div class="block">
+			<div class="block">
+			<div class="block">
+			<div class="block">
+			<div class="block">
+		</section>
+		<footer>
+			<img class="logo-icon" src="" alt=""/>
+			<div class="footer-bar">
+				<div class="site-info">
+			</div>
+			<div class="social-links">
+				<a href=""><img src="" alt="Facebook"/></a>
+				<a href=""><img src="" alt="Twitter"/></a>
+				<a href=""><img src="" alt="Pinterest"/></a>
+				<a href=""><img src="" alt="Instagram"/></a>
+			</div>
+			<a href="">About</a>
+			<a href="">Contact</a>
+		</footer>
+	</div>
+</body>
+</html>
