538190d83526645ef75b28be9a415ec4a660d113
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-10-31 09:14:13 +1000

    Re-add favicon and set base href

--- bl-themes/heirloom-earth/php/_head.php
+++ bl-themes/heirloom-earth/php/_head.php
@@ -1,3 +1,5 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="<?= HTML_PATH_THEME_CSS ?>main.scss"/>
+<link rel="shortcut icon" href="<?= HTML_PATH_THEME_IMG ?>icon.ico"/>
+<base href="/"/>
