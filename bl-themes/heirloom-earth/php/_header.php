1c2f8bfca45a10bfba7a7e636109e7e07cb61179
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-10-31 13:56:07 +1000

    Prevent custom cover images being displayed on home page

--- bl-themes/heirloom-earth/php/_header.php
+++ bl-themes/heirloom-earth/php/_header.php
@@ -1,4 +1,5 @@
-<header class="<?= $Page->coverImage() ? "has-cover" : "" ?>">
+<?php $cover = ($Url->whereAmI() == "page" and $Page->coverImage()) ?>
+<header class="<?= $cover ? "has-cover" : "" ?>">
 	<img class="logo" src="<?= HTML_PATH_THEME_IMG ?>header-logo.svg" alt="Heirloom Earth"/>
 	<nav>
 		<a class="is-active" href="">Blog</a>
