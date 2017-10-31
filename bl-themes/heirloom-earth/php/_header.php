8d362730653ba4d2da35a23ab94353028b9eea3f
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-10-31 13:56:42 +1000

    Set URLs for nav menu links

--- bl-themes/heirloom-earth/php/_header.php
+++ bl-themes/heirloom-earth/php/_header.php
@@ -3,8 +3,8 @@
 	<img class="logo" src="<?= HTML_PATH_THEME_IMG ?>header-logo.svg" alt="Heirloom Earth"/>
 	<nav>
 		<a class="is-active" href="">Blog</a>
-		<a href="">Categories</a>
-		<a href="">About</a>
-		<a href="">Contact</a>
+		<a href="category">Categories</a>
+		<a href="about">About</a>
+		<a href="contact">Contact</a>
 	</nav>
 </header>
