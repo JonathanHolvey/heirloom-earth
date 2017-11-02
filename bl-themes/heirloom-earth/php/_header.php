25e70e5780003db2607f0923f6b56c5d81c4824d
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-11-02 21:19:17 +1000

    Add logic to control active navigation link and adjust link spacing

--- bl-themes/heirloom-earth/php/_header.php
+++ bl-themes/heirloom-earth/php/_header.php
@@ -1,10 +1,22 @@
-<?php $cover = ($Url->whereAmI() == "page" and $Page->coverImage()) ?>
+<?php 
+	$cover = ($Url->whereAmI() == "page" and $Page->coverImage());
+	// Set variable to control active nav menu link
+	if ($Url->whereAmI() == "home")
+		$menu = "home";
+	elseif ($Url->whereAmI() == "page" and $Page->slug() == "about")
+		$menu = "about";
+	elseif ($Url->whereAmI() == "page" and $Page->slug() == "contact")
+		$menu = "contact";
+	elseif ($Page->title() != "Page not found")
+		$menu = "blog";
+?>
 <header class="<?= $cover ? "has-cover" : "" ?>">
 	<img class="logo" src="<?= HTML_PATH_THEME_IMG ?>header-logo.svg" alt="Heirloom Earth"/>
 	<nav>
-		<a class="is-active" href="">Blog</a>
-		<a href="category">Categories</a>
-		<a href="about">About</a>
-		<a href="contact">Contact</a>
+		<a class="<?= $menu == "home" ? "is-active" : "" ?>" href="">Home</a>
+		<a class="<?= $menu == "blog" ? "is-active" : "" ?>" href="<?= $pages[0]->slug() ?>">Blog</a>
+		<a class="<?= $menu == "category" ? "is-active" : "" ?>" href="category">Categories</a>
+		<a class="<?= $menu == "about" ? "is-active" : "" ?>" href="about">About</a>
+		<a class="<?= $menu == "contact" ? "is-active" : "" ?>" href="contact">Contact</a>
 	</nav>
 </header>
