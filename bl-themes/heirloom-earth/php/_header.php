0348ec5e6daccca786730add23b8682757f1792f
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-11-07 13:50:52 +1000

    Fix blog nav link url and clean not found detection

--- bl-themes/heirloom-earth/php/_header.php
+++ bl-themes/heirloom-earth/php/_header.php
@@ -1,5 +1,6 @@
 <?php 
 	$cover = ($Url->whereAmI() == "page" and $Page->coverImage());
+	$latest = buildPage(array_keys($dbPages->getList(1, 1, true))[0]);
 	// Set variable to control active nav menu link
 	if ($Url->whereAmI() == "home")
 		$menu = "home";
@@ -7,14 +8,14 @@
 		$menu = "about";
 	elseif ($Url->whereAmI() == "page" and $Page->slug() == "contact")
 		$menu = "contact";
-	elseif ($Page->title() != "Page not found")
+	elseif (!$Url->notFound())
 		$menu = "blog";
 ?>
 <header class="<?= $cover ? "has-cover" : "" ?>">
 	<img class="logo" src="<?= HTML_PATH_THEME_IMG ?>header-logo.svg" alt="Heirloom Earth"/>
 	<nav>
 		<a class="<?= $menu == "home" ? "is-active" : "" ?>" href="">Home</a>
-		<a class="<?= $menu == "blog" ? "is-active" : "" ?>" href="<?= $pages[0]->slug() ?>">Blog</a>
+		<a class="<?= $menu == "blog" ? "is-active" : "" ?>" href="<?= $Site->uriFilters("page") . $latest->slug() ?>">Blog</a>
 		<a class="<?= $menu == "category" ? "is-active" : "" ?>" href="category">Categories</a>
 		<a class="<?= $menu == "about" ? "is-active" : "" ?>" href="about">About</a>
 		<a class="<?= $menu == "contact" ? "is-active" : "" ?>" href="contact">Contact</a>
