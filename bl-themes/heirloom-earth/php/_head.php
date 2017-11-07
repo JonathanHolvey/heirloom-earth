860809f052eccc11957bfd128d96a6310936cda8
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-11-07 13:52:21 +1000

    Set HTML base tag to use site config for href

--- bl-themes/heirloom-earth/php/_head.php
+++ bl-themes/heirloom-earth/php/_head.php
@@ -2,4 +2,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="<?= HTML_PATH_THEME_CSS ?>main.scss"/>
 <link rel="shortcut icon" href="<?= HTML_PATH_THEME_IMG ?>icon.ico"/>
-<base href="/"/>
+<base href="<?= HTML_PATH_ROOT ?>"/>
