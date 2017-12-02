1ad445190e01e85ba45eab6d6fa9dd272b92f986
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-12-02 12:45:53 +1000

    Prevent category page from returning 404 code

--- bl-themes/heirloom-earth/index.php
+++ bl-themes/heirloom-earth/index.php
@@ -1,13 +1,14 @@
 <?php
-	include(THEME_DIR_PHP . "_functions.php");
+include(THEME_DIR_PHP . "_functions.php");
 
-	if ($Url->whereAmI() == "home")
-		include_once(THEME_DIR_PHP . "home.php");
-	elseif ($Url->whereAmI() == "page") {
-		if ($Url->uri() == "/category")
-			include_once(THEME_DIR_PHP . "category-list.php");
-		else
-			include_once(THEME_DIR_PHP . "page.php");
-	}
-	elseif ($Url->whereamI() == "category" or $Url->whereamI() == "tag")
-		include_once(THEME_DIR_PHP . "post-list.php");
+if ($Url->whereAmI() == "home")
+	include_once(THEME_DIR_PHP . "home.php");
+elseif ($Url->uri() == "/category") {
+	http_response_code(200);
+	include_once(THEME_DIR_PHP . "category-list.php");
+}
+elseif ($Url->whereAmI() == "page") {
+	include_once(THEME_DIR_PHP . "page.php");
+}
+elseif ($Url->whereamI() == "category" or $Url->whereamI() == "tag")
+	include_once(THEME_DIR_PHP . "post-list.php");
