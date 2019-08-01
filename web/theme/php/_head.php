659df67581338bd6021d19cb4ff4a858bba649ef
Jonathan Holvey <jonathan.holvey@outlook.com>
2019-08-01 18:46:21 +1000

    Update path for Webpack-built CSS file

--- web/theme/php/_head.php
+++ web/theme/php/_head.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="<?= HTML_PATH_THEME_CSS ?>main.scss?v=<?= THEME_VERSION ?>"/>
+<link rel="stylesheet" type="text/css" href="<?= HTML_PATH_THEME_CSS ?>main.css?v=<?= THEME_VERSION ?>"/>
 <link rel="shortcut icon" href="<?= HTML_PATH_THEME_IMG ?>icon.ico"/>
 <link rel="canonical" href="<?= $canonical ?>"/>
 <base href="<?= HTML_PATH_ROOT ?>"/>
